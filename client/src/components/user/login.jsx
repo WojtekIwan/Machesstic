d8678624b080768ee81da47b67b0755672585bdd
WojtekIwan <wojtekiwan1501@gmail.com>
2026-03-26 20:04:54 +0100

    Creating simple form for logging in

--- client/src/components/user/login.jsx
+++ client/src/components/user/login.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
+import {Link} from "react-router-dom"
+
 function Login(){
+    const [usernameOrEmail, setUsernameOrEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function check_login_data(e){
+        e.preventDefault()
+        console.log(`Email or username: ${usernameOrEmail} Password: ${password}`)
+    }
+
     return (
         <div>
-            <h2>This is login page - currently under construction</h2>
+            {/* Logging in form */}
+            <div>
+                <h2>Log into Machesstic</h2>
+
+                <label htmlFor="usernameOrPassowrd">Enter your email or username:</label>
+                <input type="text" id="usernameOrPassowrd" onChange={(e) => setUsernameOrEmail(p => e.target.value)}/>
+
+                <label htmlFor="password">Enter your password:</label>
+                <input type="password" id="password" onChange={(e) => setPassword(p => e.target.value)}/>
+
+                <button onClick={(e) => check_login_data(e)}>Log into your account</button>
+            </div>
         </div>
     )
 }
