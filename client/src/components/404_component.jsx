179b26507c73b9890c3e508b77c964ccb2a4d1cb
WojtekIwan <wojtekiwan1501@gmail.com>
2026-04-02 11:34:49 +0200

    Adding basic styles to 404 error site

--- client/src/components/404_component.jsx
+++ client/src/components/404_component.jsx
@@ -1,7 +1,15 @@
+import "../styles/main.scss"
+import {Link} from "react-router-dom"
+import lostGif from "../assets/404.gif"
+
 function Error(){
     return (
-        <div>
-            <h2>404 - page not found</h2>
+        <div id="error-component">
+            <div>
+                <img src={lostGif} alt="Lost gif" />
+                <h2>404 - page not found</h2>
+                <Link to={"/"} className="link">Go back to main page</Link>
+            </div>
         </div>
     )
 }
