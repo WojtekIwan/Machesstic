5dd2f2e507764c513737c0b97924d75b82541120
WojtekIwan <wojtekiwan1501@gmail.com>
2026-08-04 09:24:12 +0200

    refactor: updated login styles and login page

--- client/src/components/user/login.jsx
+++ client/src/components/user/login.jsx
@@ -1,10 +1,16 @@
+// ***********************************************************************************
+//                                   LOGIN COMPONENT
+//          It`s responsible for user logging in and validating login data
+// ***********************************************************************************
+
+// Imports
 import { useState } from "react"
 import {Link, useNavigate} from "react-router-dom"
 import axios from "axios"
 import "../../styles/main.scss"
 import logo from "../../assets/logo.png"
 
-function Login(){
+export default function Login(){
     const [usernameOrEmail, setUsernameOrEmail] = useState("")
     const [password, setPassword] = useState("")
 
@@ -12,29 +18,30 @@ function Login(){
 
     const navigate = useNavigate()
 
+    // Sending data to backend and validate it
     function check_login_data(e){
-        e.preventDefault()
-        console.log(`Email or username: ${usernameOrEmail} Password: ${password}`)
+        e.preventDefault() // Not redirecting to another page yet
+        // Sending request to log user in
         axios.post("http://localhost:3000/user/login", {"usernameOrEmail": usernameOrEmail, "password": password}).then((res) => {
-            if(res.data.code == 200){
-                send_request_to_create_jwt_token(res.data.data[0].id)
-            }else{
-                setErrorMessage(p=> res.data.message)
-                console.log(res.data, res.data.message)
-            }
+            send_request_to_create_jwt_token(res.data.id)
+        }).catch(error => {
+            // Catch error from server (wrong password, not filling every input etc)
+            setErrorMessage(p=> error.response.status == 400 ? error.response.data.message : "Unexpected error")
         })
     }
 
+    // Sending request for creating jwt token
     function send_request_to_create_jwt_token(user_id){
         axios.post("http://localhost:3000/user/create_jwt", {"user_id": user_id}, {withCredentials: true}).then(res => {
-            console.log(res.status, res.data, " <- tokens where created here")
-            navigate("/user")
+            navigate("/user") // After creating tokens navigate to user main page
+        }).catch((err) => {
+            setErrorMessage(p=> "Unexpected error")
         })
     }
 
+    // Component render
     return (
         <div id="log-in-form">
-            {/* Logging in form */}
             <div>
                 <img src={logo} alt="Logo for Machesstic" />
 
@@ -53,5 +60,3 @@ function Login(){
         </div>
     )
 }
-
-export default Login
