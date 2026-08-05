5910290a77d1c48b00e5df22ab5a10903b09a3f5
WojtekIwan <wojtekiwan1501@gmail.com>
2026-08-05 09:31:25 +0200

    feat: small logger component for prettier logs

---
+++ server/logger.js
@@ -0,0 +1,31 @@
+// ***********************************************************************************
+//                                     LOGGER
+//                    Prints prettier logs (I was really bored)
+// ***********************************************************************************
+
+export default class Logger{
+    // White bold text 
+    title(message){
+        console.log("\x1b[1m%s\x1b[0m", `[📢 ] ${message}`)
+    }
+
+    // Red text
+    error(message){
+        console.log("\x1b[31m%s\x1b[0m", `[❌ ] ${message}`)
+    }
+
+    // Yellow text
+    warning(message){
+        console.log("\x1b[33m%s\x1b[0m", `[⚠️ ] ${message}`)
+    }
+
+    // Green text
+    okay(message){
+        console.log("\x1b[32m%s\x1b[0m", `[✅ ] ${message}`)
+    }
+
+    // White text
+    default_message(message){
+        console.log(`[📃 ] ${message}`)
+    }
+}
